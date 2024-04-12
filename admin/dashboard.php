196a1b3ef6ff0899e5a9928aabac580bad7faaf5
9rato <moshood.ajose6@gmail.com>
2024-04-12 15:52:01 +0000

    new projectjlllooooouuuoooo

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -160,7 +160,28 @@ mysqli_close($conn);
                             </div>
                             <div class="flex-1 text-right md:text-center">
                                 <h5 class="font-bold uppercase text-gray-500">Total Revenue</h5>
-                                <h3 class="font-bold text-3xl">N3249 <span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
+                            
+                                <?php
+                                //naira code
+                                include('./db/config.php');
+                                // SQL query to calculate total balance
+                                $sql = "SELECT SUM(Naira_Balance) AS total_balance FROM naira";
+
+                                $result = $conn->query($sql);
+
+                                if ($result->num_rows > 0) {
+                                    // Output data of each row
+                                    while ($row = $result->fetch_assoc()) {
+                                        $total_balance = $row["total_balance"];
+                                    }
+                                } else {
+                                    $total_balance = 0;
+                                }
+
+                                $conn->close();
+                                ?>
+
+                                <h3 class="font-bold text-3xl">₦<?php echo number_format($total_balance, 2); ?><span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
                             </div>
                         </div>
                     </div>
